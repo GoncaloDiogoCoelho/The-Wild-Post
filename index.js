73b90437a5e56bdcbed13e5fd5f6054753cdf539
Carla Gonçalves <goncalves.carla1998@gmail.com>
2022-09-29 12:30:39 +0100

    Do some changes in the carousel section and add the code that will create the topic section where the articles will be

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//*Image Carousel
 // Creation of the section that will contain the carousel images / articles
 const carousel = document.createElement("section");
 carousel.classList.add("carousel");
@@ -7,35 +8,43 @@ main.appendChild(carousel);
 const slidesContent = [
     {
         number: "1/3",
-        picture: ,
-        alternative: ,
+        title: "",
+        picture: "",
+        alternative: "",
         description: ""
     }, 
     {
         number: "2/3",
-        picture: ,
-        alternative: ,
+        title: "",
+        picture: "",
+        alternative: "",
         description: ""
     }, 
     {
         number: "3/3",
-        picture: ,
-        alternative: ,
+        title: "",
+        picture: "",
+        alternative: "",
         description: ""
     }
 ];
 
-function createSlides(numbers, imageURL, alternativeText, text) {
+function createSlides(numbers, mainTitle, imageURL, alternativeText, text) {
 //Creation of the slides itself
     const slides = document.createElement("div");
     slides.classList.add("slides");
     carousel.appendChild(slides);
 
 //Creation of the content inside the slides
-    const numbering = document.createElement("div");
+    const numbering = document.createElement("p");
     numbering.classList.add("numbering");
     numbering.innerHTML = `${numbers}`;
     slides.appendChild(numbering);
+
+    const slideTitle = document.createElement("h1");
+    slideTitle.classList.add("slide-title");
+    slideTitle.innerHTML = `${mainTitle}`;
+    slides.appendChild(slideTitle);
 
     const carouselImages = document.createElement("img");
     carouselImages.classList.add("carousel-images");
@@ -43,7 +52,7 @@ function createSlides(numbers, imageURL, alternativeText, text) {
     carouselImages.alt = `${alternativeText}`;
     slides.appendChild(carouselImages);
 
-    const subtitles = document.createElement("div");
+    const subtitles = document.createElement("p");
     subtitles.classList.add("subtitles");
     subtitles.innerHTML = `${text}`;
     slides.appendChild(subtitles);
@@ -51,5 +60,76 @@ function createSlides(numbers, imageURL, alternativeText, text) {
 
 //This loop will repeat the function above for each slide and respective content
 for (let i = 0; i < slidesContent.length; i++) {
-    createSlides(slidesContent[i].number, slidesContent[i].picture, slidesContent[i].alternative, slidesContent[i].text);
+    createSlides(slidesContent[i].number, slidesContent[i].title, slidesContent[i].picture, slidesContent[i].alternative, slidesContent[i].text);
+};
+
+
+
+//*Topic section
+// Creation of the section that will contain the topics and respective articles
+const topics = document.createElement("section");
+topics.classList.add("topics-container");
+main.appendChild(topics);
+
+//Object with the contents of each topic
+const topicsContent = [
+    {
+        topicTitle: "",
+        articleTitle: "",
+        picture: "",
+        alternative: "",
+        description: ""
+    }, 
+    {
+        topicTitle: "",
+        articleTitle: "",
+        picture: "",
+        alternative: "",
+        description: ""
+    }, 
+    {
+        topicTitle: "",
+        articleTitle: "",
+        picture: "",
+        alternative: "",
+        description: ""
+    }
+];
+
+function createTopics(topicTitle, imageLink, altText, articleTitle, paragraphs) {
+    //Creation of one topic section and respective title
+        const oneTopic = document.createElement("div");
+        oneTopic.classList.add("topic");
+        topics.appendChild(oneTopic);
+
+        const oneTopicTitle = document.createElement("h2");
+        oneTopicTitle.classList.add("topic-title");
+        oneTopicTitle.innerHTML = `${topicTitle}`;
+        oneTopic.appendChild(oneTopicTitle);
+    
+    //Creation of the content inside each topic
+        const topicBox = document.createElement("div");
+        topicBox.classList.add("topic-box");
+        oneTopic.appendChild(topicBox);
+
+        const articleImages = document.createElement("img");
+        articleImages.classList.add("article-images");
+        articleImages.src = `${imageLink}`;
+        articleImages.alt = `${altText}`;
+        topicBox.appendChild(articleImages);
+
+        const reportTitle = document.createElement("h3");
+        reportTitle.classList.add("slide-title");
+        reportTitle.innerHTML = `${articleTitle}`;
+        topicBox.appendChild(reportTitle);
+    
+        const articleContent = document.createElement("p");
+        articleContent.classList.add("article-content");
+        articleContent.innerHTML = `${paragraphs}`;
+        topicBox.appendChild(subtitles);
+    };
+
+//This loop will repeat the function above for each slide and respective content
+for (let j = 0; j < topicsContent.length; j++) {
+    createTopics(topicsContent[j].topicTitle, topicsContent[j].picture, topicsContent[j].alternative, topicsContent[j].articleTitle, topicsContent[j].description);
 };
