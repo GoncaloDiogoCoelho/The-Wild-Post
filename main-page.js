5e6b04c42dccb4055bcacb9336718ce71b3edf08
Carla Gonçalves <goncalves.carla1998@gmail.com>
2022-09-29 16:01:20 +0100

    Add code that changes the slide when we click the arrows or the dot's below the slides

--- main-page.js
+++ main-page.js
@@ -1,8 +1,15 @@
 //*Image Carousel
-// Creation of the section that will contain the carousel images / articles
+//Creation of the section that will contain the carousel images / articles and the dots to choose the slide we want to see
+const main = document.querySelector("main");
+
 const carousel = document.createElement("section");
 carousel.classList.add("carousel");
 main.appendChild(carousel);
+
+const dotBox = document.createElement("div");
+dotBox.classList.add("dot-box");
+main.appendChild(dotBox);
+
 
 //Object with the contents of each slide
 const slidesContent = [
@@ -29,13 +36,27 @@ const slidesContent = [
     }
 ];
 
+//Creation of the slides and it's content
 function createSlides(numbers, mainTitle, imageURL, alternativeText, text) {
-//Creation of the slides itself
+    //Creation of the slides itself
     const slides = document.createElement("div");
     slides.classList.add("slides");
     carousel.appendChild(slides);
 
-//Creation of the content inside the slides
+    /*Creation of the back and foreword buttons
+    const leftButton = document.createElement("a");
+    leftButton.classList.add("left-button");
+    leftButton.onclick = moveSlides(-1);
+    leftButton.innerHTML = "&#10094;";
+    slides.appendChild(leftButton);
+
+    const rightButton = document.createElement("a");
+    rightButton.classList.add("right-button");
+    rightButton.onclick = moveSlides(1);
+    rightButton.innerHTML = "&#10095;";
+    slides.appendChild(rightButton);*/
+
+    //Creation of the content inside the slides
     const numbering = document.createElement("p");
     numbering.classList.add("numbering");
     numbering.innerHTML = `${numbers}`;
@@ -56,13 +77,55 @@ function createSlides(numbers, mainTitle, imageURL, alternativeText, text) {
     subtitles.classList.add("subtitles");
     subtitles.innerHTML = `${text}`;
     slides.appendChild(subtitles);
+
+    /*Creation of the dots below the slides to show the slide we choose 
+    const individualDot = document.createElement("div");
+    individualDot.classList.add("dot");
+    individualDot.onclick = currentSlide();
+    dotBox.appendChild(individualDot);*/
 };
+
 
 //This loop will repeat the function above for each slide and respective content
 for (let i = 0; i < slidesContent.length; i++) {
     createSlides(slidesContent[i].number, slidesContent[i].title, slidesContent[i].picture, slidesContent[i].alternative, slidesContent[i].text);
 };
 
+//Function that allow to show the current slide or move them back and foreword
+/*let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dot = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  
+  dots[slideIndex-1].className += " active";
+} */
 
 
 //*Topic section
