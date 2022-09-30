ed6acead3cf891b4af8fe926f9c3b8604f70b74a
GonçaloDiogoCoelho <goncalodmvm.c@gmail.com>
2022-09-30 09:27:13 +0100

    burger menu almost done

---
+++ script.js
@@ -0,0 +1,10 @@
+let menuButton = document.querySelector(".toggle-menu");
+let resourceButton = document.querySelector(".toggle-resources-check")
+let resourceMenu = document.querySelector(".toggle-resources")
+
+if (menuButton = false){
+    resourceMenu.style.opacity = 0;
+};
+
+
+
