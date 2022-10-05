3ba48d2076dcfcb5dcc6828da94b58e05da391fa
Carla Gonçalves <goncalves.carla1998@gmail.com>
2022-10-05 12:17:49 +0100

    Changes in the CSS of the main page and link added to the logo

--- main-page.js
+++ main-page.js
@@ -37,7 +37,7 @@ const topicsContent = [
     }
 ];
 
-function createTopics(NewsLink, topicTitle, imageLink, altText, articleTitle, paragraph, paragraphTwo) {
+function createTopics(topicTitle, imageLink, altText, articleTitle, paragraph, paragraphTwo, NewsLink) {
     //Creation of one topic section and respective title
         const oneTopic = document.createElement("div");
         oneTopic.classList.add("topic");
@@ -53,15 +53,11 @@ function createTopics(NewsLink, topicTitle, imageLink, altText, articleTitle, pa
         topicBox.classList.add("topic-box");
         oneTopic.appendChild(topicBox);
 
-        const topicLink = document.createElement("a");
-        topicLink.href = `${NewsLink}`
-        topicBox.appendChild(topicLink);
-
         const articleImages = document.createElement("img");
         articleImages.classList.add("article-images");
         articleImages.src = `${imageLink}`;
         articleImages.alt = `${altText}`;
-        topicLink.appendChild(articleImages);
+        topicBox.appendChild(articleImages);
 
         const textBox = document.createElement("div");
         textBox.classList.add("text-box");
@@ -81,11 +77,17 @@ function createTopics(NewsLink, topicTitle, imageLink, altText, articleTitle, pa
         articleContentTwo.classList.add("article-content-hide");
         articleContentTwo.innerHTML = `${paragraphTwo}`;
         textBox.appendChild(articleContentTwo);
+        
+        const topicLink = document.createElement("a");
+        topicLink.classList.add("read-more");
+        topicLink.href = `${NewsLink}`;
+        topicLink.innerHTML = "Read more &#8594;";
+        textBox.appendChild(topicLink);
     };
 
 //This loop will repeat the function above for each slide and respective content
 for (let j = 0; j < topicsContent.length; j++) {
-    createTopics(topicsContent[j].articleLink, topicsContent[j].topicTitle, topicsContent[j].picture, topicsContent[j].alternative, topicsContent[j].articleTitle, topicsContent[j].description, topicsContent[j].descriptionTwo);
+    createTopics(topicsContent[j].topicTitle, topicsContent[j].picture, topicsContent[j].alternative, topicsContent[j].articleTitle, topicsContent[j].description, topicsContent[j].descriptionTwo, topicsContent[j].articleLink);
 };
 
 
