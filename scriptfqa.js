97dc1771b595c578c9b6809059adc172e36d2554
NunoMatosCorreia <NUNO CORREIA>
2022-10-05 14:59:44 +0100

    style mobile faq page and contactus page

--- scriptfqa.js
+++ scriptfqa.js
@@ -54,7 +54,7 @@ for (i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function () {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
-        this.classList.toggle("active");
+        this.classList.toggle("activeus");
         /* Toggle between hiding and showing the active panel */
         let body = this.nextElementSibling;
         if (body.style.display === "block") {
